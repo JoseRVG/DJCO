d2279fab7d7aa0fddb9bc7883760ad38f0e846c9
ricardolopes8e <ricardolopes8e@gmail.com>
2020-03-07 18:53:55 +0000

    UI improved, grades script not working

---
+++ Assets/Scripts/Gameplay/Grades.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int startingGrades=6;
+    public Text textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = startingGrades.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
