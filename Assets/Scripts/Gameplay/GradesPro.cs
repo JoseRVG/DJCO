7efa2a8edeb473fb53a4ed64bb1194ab16c06ad4
Jose Gomes <zedeco2010@hotmail.com>
2020-03-09 11:32:47 +0000

    Bugs Fiexed
    Bugs with runing and game over fixed

---
+++ Assets/Scripts/Gameplay/GradesPro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using Platformer.Mechanics;
+
+public class GradesPro : MonoBehaviour
+{
+
+    public TextMeshProUGUI textBox;
+
+    public PlayerController player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        textBox.text = player.grades.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textBox.text = player.grades.ToString();
+    }
+}
