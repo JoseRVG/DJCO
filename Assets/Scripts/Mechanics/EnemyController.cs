8aa9fac07f477564edd99fba4d2e92e3426cfd58
Jose Gomes <zedeco2010@hotmail.com>
2020-03-12 13:35:39 +0000

    Enemys not falling
    Enemys not falling from ladders working with a bugs on their speed

--- Assets/Scripts/Mechanics/EnemyController.cs
+++ Assets/Scripts/Mechanics/EnemyController.cs
@@ -4,14 +4,12 @@ using Platformer.Gameplay;
 using UnityEngine;
 using static Platformer.Core.Simulation;
 
-namespace Platformer.Mechanics
-{
+namespace Platformer.Mechanics {
     /// <summary>
     /// A simple controller for enemies. Provides movement control over a patrol path.
     /// </summary>
-    [RequireComponent(typeof(AnimationController), typeof(Collider2D))]
-    public class EnemyController : MonoBehaviour
-    {
+    [RequireComponent (typeof (AnimationController), typeof (Collider2D))]
+    public class EnemyController : MonoBehaviour {
         public PatrolPath path;
         public AudioClip ouch;
 
@@ -20,38 +18,39 @@ namespace Platformer.Mechanics
         internal Collider2D _collider;
         internal AudioSource _audio;
         SpriteRenderer spriteRenderer;
-
+        public bool onLadder = false;
         public Bounds Bounds => _collider.bounds;
 
-       // public bool onLadder=true;
+        // public bool onLadder=true;
 
-        void Awake()
-        {
-            control = GetComponent<AnimationController>();
-            _collider = GetComponent<Collider2D>();
-            _audio = GetComponent<AudioSource>();
-            spriteRenderer = GetComponent<SpriteRenderer>();
+        void Awake () {
+            control = GetComponent<AnimationController> ();
+            _collider = GetComponent<Collider2D> ();
+            _audio = GetComponent<AudioSource> ();
+            spriteRenderer = GetComponent<SpriteRenderer> ();
         }
 
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            var player = collision.gameObject.GetComponent<PlayerController>();
-            if (player != null)
-            {
-                var ev = Schedule<PlayerEnemyCollision>();
+        void OnCollisionEnter2D (Collision2D collision) {
+            var player = collision.gameObject.GetComponent<PlayerController> ();
+            if (player != null) {
+                var ev = Schedule<PlayerEnemyCollision> ();
                 ev.player = player;
                 ev.enemy = this;
             }
         }
 
-        void Update()
-        {
-            if (path != null)
-            {
-                if (mover == null) mover = path.CreateMover(control.maxSpeed * 0.5f);
-                control.move.x = Mathf.Clamp(mover.Position.x - transform.position.x, -1, 1);
+        void Update () {
+            if (path != null) {
+                if (mover == null) mover = path.CreateMover (control.maxSpeed * 0.5f);
+                control.move.x = Mathf.Clamp (mover.Position.x - transform.position.x, -1, 1);
             }
-            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+            if (onLadder) {
+                GetComponent<Rigidbody2D> ().gravityScale = 0f;
+                control.maxSpeed = control.maxSpeed * 0.5f;
+            } else if (!onLadder) {
+                GetComponent<Rigidbody2D> ().gravityScale = 1f;
+            }
+            transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, 0);
         }
 
     }
--- Assets/Scripts/Mechanics/Ladders.cs
+++ Assets/Scripts/Mechanics/Ladders.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 namespace Platformer.Mechanics {
     public class Ladders : MonoBehaviour {
         private PlayerController player;
+        private EnemyController enemy;
         // Start is called before the first frame update
         void Start () {
             player = FindObjectOfType<PlayerController> ();
+            enemy = FindObjectOfType<EnemyController> ();
         }
 
         // Update is called once per frame
         void OnTriggerEnter2D (Collider2D other) {
             if (other.tag == "Player") {
                 player.onLadder = true;
+            } else if (other.tag == "Enemy") {
+                enemy.onLadder = true;
             }
         }
 
         void OnTriggerExit2D (Collider2D other) {
             if (other.tag == "Player") {
                 player.onLadder = false;
+            } else if (other.tag == "Enemy") {
+                enemy.onLadder = false;
             }
         }
     }
